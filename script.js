e422f0069552977b8789d865c5796d88b75672b3
Radisfox <sar.legoupil@gmail.com>
2026-07-20 16:37:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,9 @@ function checkAnswer2() {
 
         document.getElementById("info").innerHTML = `
 
-<h2>Dia de los Amigos</h2>
+<h2>ARCHIVE 2</h2> 
+<h3>Dia de los Amigos</h3> 
+<h4>20 juillet</h4>
 
 <p>
 
@@ -120,15 +122,20 @@ function checkAnswer3() {
         .replace(/[\u0300-\u036f]/g, "");
 
     let correct = "men, their rights and nothing more. women, their rights and nothing less."
+        .replace(/\s+/g, " ")
         .toLowerCase()
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "");
-
+    
+    answer = answer.replace(/\s+/g, " ");
+    
     if (answer === correct) {
 
         document.getElementById("info").innerHTML = `
 
-<h2>Première convention des droits des femmes</h2>
+<h2>ARCHIVE 3</h2> 
+<h3>Première convention des droits des femmes</h3> 
+<h4>20 juillet 1848</h4>
 
 <p>
 
@@ -158,29 +165,29 @@ Le mot d'ordre devenu l'un des plus célèbres du mouvement est :
 
 <p>
 
-<strong>Well... It's About Time you found that one! 😉</strong>
+Des femmes fortes et indépendantes…
 
 <br><br>
 
-Pendant que tu as passé tout ce temps aux toilettes à faire je ne sais quoi, j'ai capté d'étranges fréquences radio.
+J'en connais une autre qui suit dans leurs pas.
 
 <br><br>
 
-Elles semblent venir de l'espace... comme c'est excitant !
+Une femme avec plein d'idées en tête et qui n'a pas peur de les mettre en œuvre.
 
 <br><br>
 
-Je crois qu'elles utilisent l'alphabet phonétique international.
+Comme un certain week-end un peu sorcier…
 
 <br><br>
 
-Voyons si tu arrives à déchiffrer le message qu'elles essaient de transmettre.
+Et bientôt quelque chose d'un peu spatial.
+
+<br><br>
+
+En parlant de spatial, ton prochain indice se cache quelque part où la Lune dévoile son plus beau profil.
 
 </p>
-
-<audio controls>
-    <source src="signal.mp3" type="audio/mpeg">
-</audio>
 
 <input
 id="answer4"
@@ -212,6 +219,93 @@ function checkAnswer4() {
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "");
 
+    let correct = "maybeeeeeeeeeeeee"
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
+    if (answer === correct) {
+
+    document.getElementById("info").innerHTML = `
+
+<h2>ARCHIVE 4</h2>  
+<h3>Apollo 11</h3> 
+<h4>20 juillet 1969</h4>
+
+<p>
+
+Well... <strong>It's About Time</strong> you found that one! 😉
+
+<br><br>
+
+Le 20 juillet 1969, les premiers êtres humains marchent sur la Lune lors de la mission Apollo 11.
+
+<br><br>
+
+Les astronautes Neil Armstrong et Buzz Aldrin deviennent les premiers à fouler le sol lunaire, tandis que Michael Collins reste en orbite à bord du module de commande.
+
+<br><br>
+
+Cet exploit marque l'une des plus grandes avancées de l'histoire de l'exploration spatiale.
+
+</p>
+
+<hr>
+
+<h2>🔓 DOSSIER SUIVANT</h2>
+
+<p>
+
+Pendant que tu as passé tout ce temps aux toilettes à faire je ne sais quoi, j'ai capté d'étranges fréquences radio.
+
+<br><br>
+
+Elles semblent venir de l'espace… comme c'est excitant !
+
+<br><br>
+
+Je crois qu'elles utilisent l'alphabet phonétique international.
+
+<br><br>
+
+Voyons si tu arrives à déchiffrer le message qu'elles essaient de transmettre.
+
+</p>
+
+<audio controls>
+    <source src="signal.mp3" type="audio/mpeg">
+</audio>
+
+<input
+id="answer5"
+type="text"
+placeholder="Tape le message décodé">
+
+<button onclick="checkAnswer5()">
+VALIDER
+</button>
+
+<p id="error5"></p>
+
+`;
+        
+    } else {
+
+        document.getElementById("error4").textContent =
+            "Ce n'est pas la bonne réponse.";
+
+    }
+
+}
+
+function checkAnswer5() {
+
+    let answer = document.getElementById("answer5").value
+        .trim()
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
     let correct = "christina yang"
         .toLowerCase()
         .normalize("NFD")
@@ -221,7 +315,9 @@ function checkAnswer4() {
 
         document.getElementById("info").innerHTML = `
 
-<h2>Sandra Oh</h2>
+<h2>ARCHIVE 5</h2> 
+<h3>Sandra Oh</h3> 
+<h4>20 juillet 1971</h4>
 
 <p>
 
@@ -287,7 +383,7 @@ Le monde a peut-être connu beaucoup de grands événements un 20 juillet…
 
     } else {
 
-        document.getElementById("error4").textContent =
+        document.getElementById("error5").textContent =
             "Ce n'est pas la bonne réponse.";
 
     }
