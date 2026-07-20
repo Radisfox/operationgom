62d73cfac439cb3e22f112a82f7b017925e1229a
Radisfox <sar.legoupil@gmail.com>
2026-07-20 16:05:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -52,9 +52,6 @@ function checkAnswer2() {
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "");
 
-    alert(answer);
-    alert(correct);
-
     if (answer === correct) {
 
         document.getElementById("archive2").innerHTML = `
@@ -93,11 +90,118 @@ Ce sont aussi les ingrédients qui te permettront de débloquer la prochaine arc
 
 </p>
 
+<input
+id="answer3"
+type="text"
+placeholder="Votre réponse">
+
+<button onclick="checkAnswer3()">
+VALIDER
+</button>
+
+<p id="error3"></p>
+
+<div id="archive3"></div>
+
 `;
 
     } else {
 
         document.getElementById("error2").textContent =
+            "Ce n'est pas la bonne réponse.";
+
+    }
+
+}
+function checkAnswer3() {
+
+    let answer = document.getElementById("answer3").value
+        .trim()
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
+    let correct = "men, their rights and nothing more. women, their rights and nothing less."
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+
+    if (answer === correct) {
+
+        document.getElementById("archive3").innerHTML = `
+
+<h2>Première convention des droits des femmes</h2>
+
+<p>
+
+Le 20 juillet 1848, la première convention consacrée aux droits des femmes se tient à Seneca Falls, aux États-Unis.
+
+<br><br>
+
+Les participantes y réclament davantage de droits, notamment l'égalité devant la loi, l'accès à l'éducation et le droit de vote.
+
+<br><br>
+
+Cet événement marque le début organisé du mouvement pour les droits des femmes.
+
+<br><br>
+
+Le mot d'ordre devenu l'un des plus célèbres du mouvement est :
+
+<br><br>
+
+<strong>"Men, their rights and nothing more; women, their rights and nothing less."</strong>
+
+</p>
+
+<hr>
+
+<h2>🔓 DOSSIER SUIVANT</h2>
+
+<p>
+
+<strong>Well... It's About Time you found that one! 😉</strong>
+
+<br><br>
+
+Pendant que tu as passé tout ce temps aux toilettes à faire je ne sais quoi, j'ai capté d'étranges fréquences radio.
+
+<br><br>
+
+Elles semblent venir de l'espace... comme c'est excitant !
+
+<br><br>
+
+Je crois qu'elles utilisent l'alphabet phonétique international.
+
+<br><br>
+
+Voyons si tu arrives à déchiffrer le message qu'elles essaient de transmettre.
+
+</p>
+
+<audio controls>
+    <source src="signal.mp3" type="audio/mpeg">
+</audio>
+
+<input
+id="answer4"
+type="text"
+placeholder="Votre réponse">
+
+<button onclick="checkAnswer4()">
+VALIDER
+</button>
+
+<p id="error4"></p>
+
+<div id="archive4"></div>
+
+`;
+
+    } else {
+
+        document.getElementById("error3").textContent =
             "Ce n'est pas la bonne réponse.";
 
     }
